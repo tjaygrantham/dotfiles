b833d9e7a68d142becff6c8a8a669fe440dda9e8
tjaygrantham <tjaygrantham@gmail.com>
2022-05-10 14:06:40 -0400

    add ghidra macos install dir

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
     set -x THEOS ~/theos
     set -x PATH ~/.cargo/bin:$PATH
+    set -x HOMEBREW_CASK_OPTS "--no-quarantine"
+    set -x GHIDRA_INSTALL_DIR (eval "brew --prefix")/Caskroom/ghidra/*/*
 end
