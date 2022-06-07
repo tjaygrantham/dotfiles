2dae79649a396069e7bd82b2a55d0c4b8450d9af
tjaygrantham <tjaygrantham@gmail.com>
2022-06-06 22:06:13 -0400

    check for brew before setting env

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     set -x THEOS ~/theos
     set -x PATH ~/.cargo/bin:$PATH
-    set -x HOMEBREW_CASK_OPTS "--no-quarantine"
-    set -x GHIDRA_INSTALL_DIR (eval "brew --prefix")/Caskroom/ghidra/*/*
+    command -vq brew
+    if test $status = 0
+        set -x HOMEBREW_CASK_OPTS "--no-quarantine"
+        set -x GHIDRA_INSTALL_DIR (eval "brew --prefix")/Caskroom/ghidra/*/*
+    end
 end
