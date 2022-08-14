883c98ff219db6bfacd2f05f0463849340c3e415
tjaygrantham <tjaygrantham@gmail.com>
2022-08-14 18:27:10 -0400

    add cargo and linuxbrew to path

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,5 @@
 if status is-interactive
     set -x THEOS ~/theos
-    set -x PATH ~/.cargo/bin:$PATH
     command -vq brew
     if test $status = 0
         set -x HOMEBREW_CASK_OPTS "--no-quarantine"
