e8f1648e78fcf73239afcf81ec07bacc70d99c89
tjaygrantham <tjaygrantham@gmail.com>
2022-09-28 10:53:57 -0400

    remove user specific paths

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,7 @@
-if status is-interactive
-    set -x THEOS ~/theos
-    command -vq brew
-    if test $status = 0
-        set -x HOMEBREW_CASK_OPTS "--no-quarantine"
-        set -x GHIDRA_INSTALL_DIR (eval "brew --prefix")/Caskroom/ghidra/*/*
-    end
+set -x THEOS $HOME/theos
+command -vq brew
+if test $status = 0
+    set -x HOMEBREW_CASK_OPTS "--no-quarantine"
+    set -x GHIDRA_INSTALL_DIR (eval "brew --prefix")/Caskroom/ghidra/*/*
 end
 fish_add_path $HOME/.cargo/bin
-fish_add_path $HOME/.spicetify
