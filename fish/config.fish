1b25d82522f7e973fd170d9e9526e70260338862
tjaygrantham <tjaygrantham@gmail.com>
2023-11-15 23:21:56 -0500

    move path instead of modifying directly

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,13 @@
-fish_add_path -P /usr/local/bin
+fish_add_path -m $HOME/.cargo/bin
+fish_add_path -m /usr/local/bin
+set -x THEOS $HOME/theos
+set -x OO_PS4_TOOLCHAIN $HOME/OpenOrbis-PS4-Toolchain
+set -x GOLDHEN_SDK $HOME/GoldHEN_Plugins_SDK
+set -x VITASDK /usr/local/vitasdk
+set -x PSPDEV /usr/local/pspdev
 if test (uname) = Darwin
-    set -x THEOS $HOME/theos
-    set -x OO_PS4_TOOLCHAIN $HOME/OpenOrbis-PS4-Toolchain
-    set -x GOLDHEN_SDK $HOME/GoldHEN_Plugins_SDK
-    set -x VITASDK /usr/local/vitasdk
-    set -x PSPDEV /usr/local/pspdev
     if test (arch) = "arm64"
-        fish_add_path -P /opt/homebrew/bin
+        fish_add_path -m /opt/homebrew/bin
     else
         set -x PYENV_ROOT $HOME/.pyenv-x86
     end
@@ -14,13 +15,17 @@ if test (uname) = Darwin
     if test $status = 0
         set -x HOMEBREW_CASK_OPTS "--no-quarantine"
         set -x GHIDRA_INSTALL_DIR (brew --prefix)/Caskroom/ghidra/*/*
-        fish_add_path -P (brew --prefix make)/libexec/gnubin
+        fish_add_path -m (brew --prefix make)/libexec/gnubin
     end
     if test -d /opt/MonkeyDev
         set -x MonkeyDevPath /opt/MonkeyDev
         set -x MonkeyDevDeviceIP localhost
         set -x MonkeyDevDevicePort 2222
-        fish_add_path -P /opt/MonkeyDev/bin
+        fish_add_path -m /opt/MonkeyDev/bin
+    end
+else
+    command -vq brew
+    if test $status = 0
+        fish_add_path -m /home/linuxbrew/.linuxbrew/bin
     end
 end
-fish_add_path -P $HOME/.cargo/bin
