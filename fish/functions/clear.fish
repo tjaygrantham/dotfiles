93bf8d32de2b32f2d537dce9f7b4e8d089163bc7
tjaygrantham <tjaygrantham@gmail.com>
2023-11-21 01:49:25 -0500

    clear scrollback in kitty

--- fish/functions/clear.fish
+++ fish/functions/clear.fish
@@ -1,5 +1,6 @@
 function clear
   /usr/bin/env clear
+  printf "\e[H\e[3J"
   set script_path ~/.config/fish/functions/clear.applescript
   command -vq osascript
   if test $status = 0 && test -f $script_path
