c9f51bddbfe458f89a96bffe078c12f493dccd74
tjaygrantham <65570095+tjaygrantham@users.noreply.github.com>
2024-06-08 22:14:26 -0400

    updates

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,6 +1,14 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if require("lazy.status").has_updates then
+      require("lazy").update({ show = false })
+    end
+  end,
+})
+
 vim.filetype.add({
   extension = {
     x = "objc",
