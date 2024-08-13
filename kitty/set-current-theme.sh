e12c4809294f6e84e98b43b527061151986bceae
tjaygrantham <65570095+tjaygrantham@users.noreply.github.com>
2024-08-13 04:13:03 -0400

    kill dark-notify on exit

---
+++ kitty/set-current-theme.sh
@@ -0,0 +1,5 @@
+if [[ $1 == 'light' ]]; then
+  kitty +kitten themes --reload-in=all Rosé Pine Dawn
+else
+  kitty +kitten themes --reload-in=all Rosé Pine Moon
+fi
--- kitty/toggle-theme.sh
+++ kitty/toggle-theme.sh
@@ -1,5 +1,8 @@
-if [[ $1 == 'light' ]]; then
-  kitty +kitten themes --reload-in=all Rosé Pine Dawn
-else
-  kitty +kitten themes --reload-in=all Rosé Pine Moon
+PATH=/opt/homebrew/bin:/usr/local/bin:$PATH
+command -v dark-notify >/dev/null
+if [[ $? -eq 0 ]]; then
+  dark-notify -c "~/.config/kitty/set-current-theme.sh" &
+  DARK_NOTIFY_PID=$!
+  lsof -p $PPID +r 1 &>/dev/null
+  kill $DARK_NOTIFY_PID
 fi
