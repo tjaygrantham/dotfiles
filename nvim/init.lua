2a94ab59d2904773d70f4cacb65e1fec43e23dfd
tjaygrantham <65570095+tjaygrantham@users.noreply.github.com>
2024-12-30 00:03:32 -0500

    check uname

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-if vim.fn.has("macunix") then
+if vim.loop.os_uname().sysname == "Darwin" then
   require("dark_notify").update()
 end
