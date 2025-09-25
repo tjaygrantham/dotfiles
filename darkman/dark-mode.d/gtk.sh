05687f8167b8adc234bd1aff09e85d81a9dc3287
tjaygrantham <65570095+tjaygrantham@users.noreply.github.com>
2025-09-25 17:46:52 -0400

    update

--- darkman/dark-mode.d/gtk.sh
+++ darkman/dark-mode.d/gtk.sh
@@ -4,4 +4,4 @@ ln -sf /usr/share/themes/rose-pine-moon-gtk/gtk-4.0/gtk.css ~/.config/gtk-4.0/gt
 gsettings set org.gnome.desktop.interface gtk-theme "rose-pine-moon-gtk"
 gsettings set org.gnome.desktop.interface icon-theme "rose-pine-moon-icons"
 gsettings set org.gnome.desktop.wm.preferences theme "rose-pine-moon-gtk"
-# gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
